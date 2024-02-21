d34a95f59fcd520b487841f31e53eb4b65559aa1
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-02-21 18:20:19 -0500

    Completed 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,19 +1,45 @@
 import { useState } from 'react'
 
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Button = ( {handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Total = ({ text, amount }) => <p>{text} {amount} </p>
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
-      {right}
+      <Header text='give feedback' />
+
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+
+      <Header text='statistics' />
+
+      <Total text='good' amount={good} />
+      <Total text='neutral' amount={neutral} />
+      <Total text='bad' amount={bad} />
     </div>
   )
 }
