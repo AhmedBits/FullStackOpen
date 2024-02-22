380e073820efe188262bdd9ae4eb21e869ba3ec2
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-02-21 19:55:15 -0500

    Completed 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,18 +2,38 @@ import { useState } from 'react'
 
 const Header = ({ text }) => <h1>{text}</h1>
 
-const Button = ( {handleClick, text }) => {
+const Button = ({ handleClick, text }) => {
   return (
     <button onClick={handleClick}>{text}</button>
   )
 }
 
-const Total = ({ text, amount }) => <p>{text} {amount} </p>
+const Total = ({ text, amount }) => <p>{text} {amount}</p>
+
+const Statistics = ({ sum, average, positive }) => {
+  return (
+    <div>
+      <p>all {sum}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const scores = [
+    good,
+    neutral,
+    bad
+  ]
+
+  const sum = scores.reduce((sum, current) => sum + current, 0)
+  const average = (good * 1) + (neutral * 0) + (bad * -1)
+  const positive = good / sum
 
   const handleGoodClick = () => {
     setGood(good + 1)
@@ -40,6 +60,8 @@ const App = () => {
       <Total text='good' amount={good} />
       <Total text='neutral' amount={neutral} />
       <Total text='bad' amount={bad} />
+
+      <Statistics sum={sum} average={average / sum} positive={positive} />
     </div>
   )
 }
