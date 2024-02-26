345706109744844b5d9e6e3f3908100024574a3b
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-02-26 18:50:25 -0500

    Completed 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,4 +1,27 @@
-import { useState } from 'react'
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )} 
+  </>
 
 const App = () => {
   const course = {
