92911f17f614a2395162f32192d185ab3212219f
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-07-19 18:14:14 -0400

    Completed 2.18

--- part2/countries/src/components/Countryinfo.jsx
+++ part2/countries/src/components/Countryinfo.jsx
@@ -1,8 +1,20 @@
 const Countryinfo = ({ country }) => {
   return (
-    <pre>
-      {JSON.stringify(country, null, 2)}
-    </pre>
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area} km²</p>
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((lang, index) => 
+          <li key={index}>{lang}</li>
+        )}
+      </ul>
+      <img 
+        src={country.flags.png}
+        alt={country.flags.alt}
+      />
+    </div>
   )
 }
 
