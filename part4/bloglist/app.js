2d773f80afbae659ae4007095837a1faf1fdc827
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-09-20 17:55:03 -0400

    Completed 4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,8 +7,13 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
