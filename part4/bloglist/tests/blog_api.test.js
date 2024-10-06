2569a4f9721c937f5b04ff9eddf8e6fe68b1bb05
AhmedBits <AhmedBits@users.noreply.github.com>
2024-10-06 18:10:44 -0400

    Structured tests within a describe block

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, describe, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -13,46 +13,48 @@ beforeEach(async () => {
   await Blog.insertMany(initialBlogs)
 })
 
-test('response in in JSON format', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('has correct number of blogs', async () => {
-  const response = await api.get('/api/blogs')
-  
-  assert.strictEqual(response.body.length, initialBlogs.length)
-})
-
-test('each blog has an "id" field', async () => {
-  const response = await api.get('/api/blogs')
-
-  response.body.forEach(blog => {
-    assert.ok(Object.hasOwn(blog, 'id'))
+describe('blog database', () => {
+  test('response is in JSON format', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
   })
-})
 
-test('successfully creates a new blog post', async () => {
-  const newBlog = {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12
-  }
+  test('has correct number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-  
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(r => r.title)
+  test('gives each blog an "id" field', async () => {
+    const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, initialBlogs.length + 1)
-  assert(titles.includes(newBlog.title))
+    response.body.forEach(blog => {
+      assert.ok(Object.hasOwn(blog, 'id'))
+    })
+  })
+
+  test('successfully creates a new blog post', async () => {
+    const newBlog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+    assert(titles.includes(newBlog.title))
+  })
 })
 
 after(async () => {
