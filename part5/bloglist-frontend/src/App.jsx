b2785d752118c06d34b73bcf1aa8f4897d70fc98
AhmedBits <AhmedBits@users.noreply.github.com>
2024-11-13 19:35:08 -0500

    Added login form when user isn't logged in

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,9 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +14,46 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log(username, password)
+  }
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button>login</button>
+        </form>
+      </div>
+    )
+  }
+
+  const blogList = () => {
+    return (
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
+
+  return user === null ? loginForm() : blogList()
 }
 
 export default App
