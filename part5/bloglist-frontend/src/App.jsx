d204fefdb498adb2d245a68ba74356c311b12241
AhmedBits <AhmedBits@users.noreply.github.com>
2024-11-16 18:14:11 -0500

    Completed 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -15,6 +15,13 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      setUser(JSON.parse(loggedUserJSON))
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,12 +29,25 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log(exception)
     }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+    console.log(window.localStorage.getItem('loggedUser'), user);
+    
   }
 
   if (user === null) {
@@ -38,12 +58,19 @@ const App = () => {
           <div>
             username
             <input
+              type="text"
+              value={username}
+              name="Username"
+              autoComplete="off"
               onChange={({ target }) => setUsername(target.value)}
             />
           </div>
           <div>
             password
             <input
+              type="password"
+              value={password}
+              name="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
@@ -56,7 +83,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <div>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
