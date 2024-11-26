3707176cdbde0665a813acc3ca9166cd9f3420ea
AhmedBits <AhmedBits@users.noreply.github.com>
2024-11-25 21:42:50 -0500

    BlogForm now contains html for blog creation

---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,30 @@
+const BlogForm = () => {
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          autoComplete="off"
+          onChange={({ target }) => setUsername(target.value)}
+          required
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+          required
+        />
+      </div>
+      <button>login</button>
+    </form>
+  )
+}
+
+export default BlogForm
