254406aa26b05f1e268cff84a0c20848b19e207e
AhmedBits <AhmedBits@users.noreply.github.com>
2025-01-06 19:58:08 -0500

    Completed 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -7,13 +7,24 @@ test('<Blog /> renders title and author, but not url and likes by default', () =
   render(<Blog blog={blog} />)
 
   const titleAndAuthor = screen.getByText('a title an author')
-  const urlAndLikes = screen.queryByText('a url 0 likes')
+  const url = screen.queryByText('a url')
+  const likes = screen.queryByText('likes 0')
+
   expect(titleAndAuthor).toBeDefined()
-  expect(urlAndLikes).toBeNull()
+  expect(url).toBeNull()
+  expect(likes).toBeNull()
 })
 
-test('<Blog /> renders url and likes when the view button is clicked', () => {
-  const mockHandler = vi.fn()
-
+test('<Blog /> renders url and likes when the view button is clicked', async () => {
   render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('View')
+  await user.click(viewButton)
+
+  const url = screen.getByText('a url')
+  const likes = screen.getByText('likes 0')
+
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
 })
