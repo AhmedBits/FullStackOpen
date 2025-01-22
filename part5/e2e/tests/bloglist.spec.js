16ee59addefc79a1443fb39b3a2ae2391921259b
AhmedBits <AhmedBits@users.noreply.github.com>
2025-01-22 17:20:19 -0500

    Completed 5.17

--- part5/e2e/tests/bloglist.spec.js
+++ part5/e2e/tests/bloglist.spec.js
@@ -2,10 +2,11 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
-    // ...
+    const locator = await page.getByText('Log in to application')
+    await expect(locator).toBeVisible()
   })
 })
